3f9b883b2acbcc64977f1e85873d2b001e79788b
Diego Bosch <diegojbosch@gmail.com>
2020-07-29 23:16:58 -0400

    Updating application name

--- application.py
+++ application.py
@@ -1,19 +1,19 @@
 from flask import Flask, jsonify, request
 import requests
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+application.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	application.run(debug=True)
 
-@app.route('/search')
+@application.route('/search')
 def homepage():
-	return app.send_static_file("index.html")
+	return application.send_static_file("index.html")
 
 #retrieve products from eBay API
-@app.route('/api/v1.0/search', methods=['GET'])
+@application.route('/api/v1.0/search', methods=['GET'])
 def get_products():
 
 	with open("token.txt", "r") as file:
@@ -22,24 +22,23 @@ def get_products():
 	ebay_api_url = 'https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=' + ebay_app_id + '&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&paginationInput.entriesPerPage=25'
 	item_filter_number = 0
 
-
 	if request.args.get('keywords') is not None:
 		ebay_api_url += '&keywords=' + request.args.get('keywords')
 
 	if request.args.get('sort_order') is not None:
 		ebay_api_url += '&sortOrder=' + request.args.get('sort_order')
 
- 	if request.args.get('max_price') is not None:
- 		ebay_api_url += '&itemFilter(' + str(item_filter_number) + ').name=MaxPrice&itemFilter(' + str(item_filter_number) + ').value=' + request.args.get('max_price') + '&itemFilter(0).paramName=Currency&itemFilter(0).paramValue=USD'
- 		item_filter_number += 1
+	if request.args.get('max_price') is not None:
+		ebay_api_url += '&itemFilter(' + str(item_filter_number) + ').name=MaxPrice&itemFilter(' + str(item_filter_number) + ').value=' + request.args.get('max_price') + '&itemFilter(0).paramName=Currency&itemFilter(0).paramValue=USD'
+		item_filter_number += 1
 
- 	if request.args.get('min_price') is not None:
- 		ebay_api_url += '&itemFilter(' + str(item_filter_number) + ').name=MinPrice&itemFilter(' + str(item_filter_number) + ').value=' + request.args.get('min_price') + '&itemFilter(1).paramName=Currency&itemFilter(1).paramValue=USD'
- 		item_filter_number += 1
+	if request.args.get('min_price') is not None:
+		ebay_api_url += '&itemFilter(' + str(item_filter_number) + ').name=MinPrice&itemFilter(' + str(item_filter_number) + ').value=' + request.args.get('min_price') + '&itemFilter(1).paramName=Currency&itemFilter(1).paramValue=USD'
+		item_filter_number += 1
 
- 	if request.args.get('return_accepted') == 'true':
- 		ebay_api_url += '&itemFilter(' + str(item_filter_number) + ').name=ReturnsAcceptedOnly&itemFilter(' + str(item_filter_number) + ').value=true'
- 		item_filter_number += 1
+	if request.args.get('return_accepted') == 'true':
+		ebay_api_url += '&itemFilter(' + str(item_filter_number) + ').name=ReturnsAcceptedOnly&itemFilter(' + str(item_filter_number) + ').value=true'
+		item_filter_number += 1
 
 	if request.args.get('free_shipping') == 'true':
 		ebay_api_url += '&itemFilter(' + str(item_filter_number) + ').name=FreeShippingOnly&itemFilter(' + str(item_filter_number) + ').value=true'
